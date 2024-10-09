681e254614b16e79b58977f20c2647d11fc50631
Daniel <carlos.chayna@tecsup.edu.pe>
2024-10-09 18:38:24 -0500

    Initial Commit

---
+++ ChaynaHuamanSoundBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChaynaHuamanSoundBoard
+//
+//  Created by Carlos Daniel Chayña Huaman on 9/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
